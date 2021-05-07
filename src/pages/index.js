0c0e4b74c95359eb4b7b0a6837b6f217bdd444f4
Nykolle Malone <malone.nykolle@gmail.com>
2021-05-07 12:01:05 -0300

    typescript and sass added

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Hellos devss!</h1>
-    </div>
-  )
-}
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/home.module.scss';
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className={styles.title}>Hellos devss!</h1>
+    </div>
+  )
+}
