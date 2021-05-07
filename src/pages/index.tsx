c1db74da994e4f650bd01451d1eb2b4a7798da93
Nykolle Malone <malone.nykolle@gmail.com>
2021-05-07 15:21:13 -0300

    global styles added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from "next/head"; 
 
-import styles from "../styles/home.module.scss";
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <h1 className={styles.title}>Hellos devss!</h1>
+      <h1>Hellos devss!</h1>
     </>
   );
 }
