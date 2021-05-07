331fbef1d33dfbb11143c04e55e081d49f395f70
Nykolle Malone <malone.nykolle@gmail.com>
2021-05-07 14:38:53 -0300

    dynamic title added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
-import styles from '../styles/home.module.scss';
+import Head from "next/head"; 
+
+import styles from "../styles/home.module.scss";
 
 export default function Home() {
   return (
-    <div>
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
       <h1 className={styles.title}>Hellos devss!</h1>
-    </div>
-  )
+    </>
+  );
 }
