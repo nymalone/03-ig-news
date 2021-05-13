ad88edc0d7196d28f7c227b6c319646f460a8bde
nymalone <malone.nykolle@gmail.com>
2021-05-13 05:57:35 -0300

    users sabed in db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,36 @@
-import NextAuth from 'next-auth';
-import Providers from 'next-auth/providers';
+import { query as q } from "faunadb";
+
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
-  // configure one or more authentication providers 
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret:process.env.GITHUB_CLIENT_SECRET,
-      scope: 'read:user'
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      scope: "read:user",
     }),
-  // more providers here if you need 
   ],
-  // a data base is optional, but required to persist accounts is database 
-  // database: 
-})
+  jwt: {
+    signingKey: process.env.SIGNING_KEY
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.Create(q.Collection("users"), {
+            data: {
+              email,
+            },
+          })
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+});
