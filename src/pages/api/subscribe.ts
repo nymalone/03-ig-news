44c1e6ffa6985dc5867912737b6263d9cc6e58cb
nymalone <malone.nykolle@gmail.com>
2021-05-17 06:16:16 -0300

    error stripe - need to fix

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,16 +1,47 @@
+import { fauna } from './../../services/fauna';
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
+import { query as q } from 'faunadb'
 import { stripe } from "../../services/stripe";
+
+type User = {
+  ref: {
+    id: string
+  }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   // só quero aceitar req do tipo POST pq eu estou criando uma checkout session do stripe
   if (req.method === "POST") {
     // quando o usuário criar na intenção de compra eu vou criar um customer pra ele dentro do painel do stripe
     const session = await getSession();
+
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(
+          q.Index('user_by_email'),
+          q.Casefold(session.user.email)
+        )
+      )
+    )
+
+
     const stripeCustomer = await stripe.customers.create({
       email: session.user.email,
       // metadata
     });
+
+    // evitar duplicação de usuário
+    await fauna.query(
+      q.Update(
+        q.Ref(q.Collection('users'), user.ref.id),
+        {
+          data: {
+            stripe_customer_id: stripeCustomer.id
+          }
+        }
+      )
+    )
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
       customer: stripeCustomer.id, // id do customer no stripe
