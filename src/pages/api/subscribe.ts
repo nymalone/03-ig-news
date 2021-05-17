046f30a79e18c69d568379f70dedf99814831c05
nymalone <malone.nykolle@gmail.com>
2021-05-17 06:22:44 -0300

    avoid duplicate user done

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,14 +1,17 @@
-import { fauna } from './../../services/fauna';
+import { fauna } from "./../../services/fauna";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
-import { query as q } from 'faunadb'
+import { query as q } from "faunadb";
 import { stripe } from "../../services/stripe";
 
 type User = {
   ref: {
-    id: string
-  }
-}
+    id: string;
+  };
+  data: {
+    stripe_customer_id: string;
+  };
+};
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   // só quero aceitar req do tipo POST pq eu estou criando uma checkout session do stripe
@@ -17,34 +20,30 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const session = await getSession();
 
     const user = await fauna.query<User>(
-      q.Get(
-        q.Match(
-          q.Index('user_by_email'),
-          q.Casefold(session.user.email)
-        )
-      )
-    )
+      q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.user.email)))
+    );
 
+    let customerId = user.data.stripe_customer_id;
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+        // metadata
+      });
 
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email,
-      // metadata
-    });
-
-    // evitar duplicação de usuário
-    await fauna.query(
-      q.Update(
-        q.Ref(q.Collection('users'), user.ref.id),
-        {
+      // evitar duplicação de usuário
+      await fauna.query(
+        q.Update(q.Ref(q.Collection("users"), user.ref.id), {
           data: {
-            stripe_customer_id: stripeCustomer.id
-          }
-        }
-      )
-    )
+            stripe_customer_id: stripeCustomer.id,
+          },
+        })
+      );
+
+      customerId = stripeCustomer.id
+    }
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id, // id do customer no stripe
+      customer: customerId, // id do customer no stripe
       payment_method_types: ["card"],
       billing_address_collection: "required",
       line_items: [
