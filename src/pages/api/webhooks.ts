9d2ef35bb3100f343cda49a57aa5b2ae1b513d93
nymalone <malone.nykolle@gmail.com>
2021-05-19 09:23:20 -0300

    wip: webhooks

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,7 +1,42 @@
+import { Stripe } from 'stripe';
 import { NextApiResponse, NextApiRequest } from 'next';
+import { Readable } from 'stream'
+import { stripe } from '../../services/stripe';
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  console.log('evento recebido')
+async function buffer(readable: Readable) {
+  const chunks = [];
 
-  res.status(200).json({ ok: true})
+  for await (const chunk of readable) {
+    chunks.push(
+      typeof chunk === 'string' ? Buffer.from(chunk) : chunk
+    );
+  }
+
+  return Buffer.concat(chunks)
+}
+
+export const config = {
+  api : {
+    bodyParser: false // desabilitar o entendimento padrão do next sobre o que está vindo da requisição 
+  }
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const buf = await buffer(req) // dentro do buf tenho a minha requisição em si 
+    const secret = req.headers['stripe-signature']
+
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET);
+    } catch (err) {
+      return res.status(400).send(`WWebhook error ${err.message}`);
+    }
+
+    res.status(200).json({ ok: true})
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method not allowed')
+  }
 }
